593128f43693c9efc8f378f1fb5b5cb76bdce619
misikir Eyob <mikireyob@gmail.com>
2020-04-06 00:32:39 -0400

    Update test_moderate_filesize.py

--- insight_testsuite/test_2/test_moderate_filesize.py
+++ insight_testsuite/test_2/test_moderate_filesize.py
@@ -2,6 +2,11 @@ import shutil
 import subprocess
 import os
 
+## Before executing this code, a large (moderate) sized csv file like
+## The one in the following link should be placed in input with filename complaints.csv
+## http://files.consumerfinance.gov/ccdb/complaints.csv.zip
+## such a file is not included in this repo in order to not make the repo unnecessarily large 
+
 path = os.getcwd()
 src = './input/complaints.csv'
 dest = path + '/../../input/complaints.csv'
